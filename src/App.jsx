51e62c2370ad983a210eef4e2d5cb2e3606821b9
Vibhav-Jaiswal <vibhavjaiswal9793@gmail.com>
2024-04-16 00:28:00 +0530

    add routes and pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,19 @@
 import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Blog from "./pages/Blog";
+import About from "./pages/About";
 
 const App = () => {
-  return <h1 className="text-3xl font-bold underline text-red-600">Hello world!</h1>;
+  return (
+    <BrowserRouter>
+     <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/blog:id" element={<Blog />} />
+      <Route path="/about" element={<About />} />
+     </Routes>
+    </BrowserRouter>
+  )
 };
 
 export default App;
