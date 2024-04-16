1352cc0aa1a6a15b024f767cd8c4cd125e9f6e2b
Vibhav-Jaiswal <vibhavjaiswal9793@gmail.com>
2024-04-16 12:55:51 +0530

    add card to home page and display data.

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,18 +1,19 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-const Card = () => {
+const Card = ({blog}) => {
+  const {title, category,image} = blog;
   return (
     <div className="border-2 sm:w-96 rounded-lg m-4 flex flex-col shadow-xl">
       <img
         className="w-96 rounded-t-lg object-cover"
-        src="https://th.bing.com/th/id/OIP.J98X6U500C2ajnNemHIHmwHaEo?w=283&h=180&c=7&r=0&o=5&dpr=1.3&pid=1.7"
+        src={image}
         alt="blog-img"
       />
       <div className="flex flex-col items-center p-4 gap-4">
-        <h1 className="text-sm uppercase">React</h1>
-        <p className="text-2xl font-semibold capitalize">
-          Redux ToolKit
+        <h1 className="text-sm uppercase">{category}</h1>
+        <p className="text-xl font-semibold capitalize line-clamp-2">
+          {title}
         </p>
         <Link to='/blog/'>
         <button className=" bg-slate-400 p-3 rounded-lg text-white hover:opacity-90 shadow-lg cursor-pointer">Learn More</button>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,28 @@
-import React from 'react'
-import Card from '../components/Card'
+import React from "react";
+import Card from "../components/Card";
+import { blogs } from "../config/data";
 
 const Home = () => {
   return (
-    <div>
-      <Card />
-    </div>
-  )
-}
+    <>
+      <div className="flex flex-col items-center justify-center">
+        <h1 className=" text-2xl sm:text-4xl lg:text-6xl font-bold text-white uppercase absolute shadow-2xl p-4">
+          Welcome to my blog
+        </h1>
+        <img
+          className="w-full h-48 sm:h-72 lg:h-96"
+          src="https://papers.co/desktop/wp-content/uploads/papers.co-vz20-lines-dark-blue-abstract-pattern-background-29-wallpaper-320x180.jpg"
+          alt="logo"
+        />
+      </div>
+      <div className="flex flex-wrap justify-between">
+        {blogs.map((blog,i) => (
+          <Card blog={blog} key={i}/>
+        ))}
+      </div>
+      ;
+    </>
+  );
+};
 
-export default Home
+export default Home;
